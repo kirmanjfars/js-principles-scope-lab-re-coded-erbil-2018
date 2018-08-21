69cdea4ea0beb55e77b3793e04e9c03aeee72813
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-08-21 12:53:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 var customerName = "bob";
 
 function upperCaseCustomerName(){
-  return customerName =  customerName.toUpperCase();
+  customerName =  customerName.toUpperCase();
+   return customerName 
 }
